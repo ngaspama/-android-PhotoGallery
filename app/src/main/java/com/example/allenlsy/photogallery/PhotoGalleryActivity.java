24c83d2935edcb019027c5ac808f9f21727ecc34
allenlsy <cafe@allenlsy.com>
2014-06-30 23:40:56 +0800

    finish 26: http request

---
+++ app/src/main/java/com/example/allenlsy/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,16 @@
+package com.example.allenlsy.photogallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return new PhotoGalleryFragment();
+    }
+}
